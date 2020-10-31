fabf89f6cb61deb14208a8b49301b50115157d78
Pedro Henrique <phsr94@gmail.com>
2020-10-31 19:57:42 -0300

    Restrição de conteúdo: cliente só recupera ele mesmo

--- src/main/java/com/pedro/cursomc/services/ClienteService.java
+++ src/main/java/com/pedro/cursomc/services/ClienteService.java
@@ -15,11 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 import com.pedro.cursomc.domain.Cidade;
 import com.pedro.cursomc.domain.Cliente;
 import com.pedro.cursomc.domain.Endereco;
+import com.pedro.cursomc.domain.enums.Perfil;
 import com.pedro.cursomc.domain.enums.TipoCliente;
 import com.pedro.cursomc.dto.ClienteDTO;
 import com.pedro.cursomc.dto.ClienteNewDTO;
 import com.pedro.cursomc.repositories.ClienteRepository;
 import com.pedro.cursomc.repositories.EnderecoRepository;
+import com.pedro.cursomc.security.UserSS;
+import com.pedro.cursomc.services.exceptions.AuthorizationException;
 import com.pedro.cursomc.services.exceptions.DataIntegrityException;
 import com.pedro.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -36,6 +39,12 @@ public class ClienteService {
 	private BCryptPasswordEncoder pe;
 
 	public Cliente find(Integer id) {
+		
+		UserSS user = UserService.authenticated();
+		if (user==null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
